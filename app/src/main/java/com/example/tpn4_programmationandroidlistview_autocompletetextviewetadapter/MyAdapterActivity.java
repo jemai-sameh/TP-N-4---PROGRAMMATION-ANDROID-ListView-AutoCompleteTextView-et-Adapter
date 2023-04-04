d42f2276015fb5f4341521772b46511ec8f3a5d3
jemai-sameh <samehjemai98@gmail.com>
2023-04-04 14:53:31 +0100

    Version 2 (CustomerAdapter)

--- app/src/main/java/com/example/tpn4_programmationandroidlistview_autocompletetextviewetadapter/MyAdapterActivity.java
+++ app/src/main/java/com/example/tpn4_programmationandroidlistview_autocompletetextviewetadapter/MyAdapterActivity.java
@@ -1,14 +1,41 @@
 package com.example.tpn4_programmationandroidlistview_autocompletetextviewetadapter;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import android.os.Bundle;
+public class MyAdapterActivity extends ArrayAdapter {
 
-public class MyAdapterActivity extends AppCompatActivity {
+    Activity context;
+    String[] items;
+
+    MyAdapterActivity(Activity c, String[] a){
+        super(c,R.layout.activity_my_adapter, a);
+        this.context = c;
+        this.items = a;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_adapter);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater i = context.getLayoutInflater();
+        View element = i.inflate(R.layout.activity_my_adapter, null);
+
+        ImageView img = (ImageView) element.findViewById(R.id.image);
+        TextView note = (TextView) element.findViewById(R.id.note);
+
+        note.setText(items[position]);
+        float valeur = Float.valueOf(items[position]);
+
+        if(valeur >= 10)
+            img.setImageResource(R.drawable.icon_mood);
+        else
+            img.setImageResource(R.drawable.icon_mood_cry);
+
+        return element;
+
     }
 }
